d897365dcd4f364eba3d7f63fd4f5ab58c41139f
Dravin Lantz <20451@my4county.net>
2021-08-25 11:18:41 -0400

    finished art/AsciiArt

---
+++ src/ascii/art/AsciiArt.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ascii.art;
+
+/**
+ *
+ * @author dlantz
+ */
+public class AsciiArt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("                  ___===-_  _-===___");
+        System.out.println("           _--^^^#####//       \\#####^^^--_");
+        System.out.println("         -^##########// (    ) \\##########^-");
+        System.out.println("       -############//  |\\^^/|  \\############-");
+        System.out.println("     _/############//   (@::@)   \\\\############\\_");
+        System.out.println("    /#############((     \\\\//     ))#############\\");
+        System.out.println("   -###############\\\\    (oo)    //###############-");
+        System.out.println("  -#################\\\\  / VV \\  //#################-");
+        //upper wings and head completed
+        System.out.println("_#/|##########/\\######(   /\\   )######/\\##########|\\#_");
+        System.out.println("|/ |#/\\#/\\#/\\/  \\#/\\##\\  |  |  /##/\\#/  \\/\\#/\\#/\\#| \\|");
+        System.out.println("`  |/  V  V  `   V  \\#\\| |  | |/#/  V   '  V  V  \\|  '");
+        System.out.println("     `   `  `      `   / | |  | | \\   '      '  '   '");
+        System.out.println("                      (  | |  | |  )");
+        System.out.println("                    __\\ | |  | | /__");
+        System.out.println("                   (vvv(VVV)(VVV)vvv)");
+        //body and legs completed. Project is now 100% completed
+    }
+    
+}
